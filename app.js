9605b09eee05ee305d0446c77c16b763eb00a592
Ankit Kumar Saw <55346806+ankitkumarsaw@users.noreply.github.com>
2021-12-08 22:49:57 +0530

    changes done

--- app.js
+++ app.js
@@ -105,3 +105,24 @@ var menulist = document.getElementById("menulist");
 
            }
        }
+
+
+const sections = document.querySelectorAll("mov");
+const navLi = document.querySelectorAll("dpg6");
+window.onscroll = () => {
+  var current = "";
+
+  sections.forEach((section) => {
+    const sectionTop = section.offsetTop;
+    if (pageYOffset >= sectionTop - 60) {
+      current = section.getAttribute("id"); }
+  });
+
+  navLi.forEach((li) => {
+    li.classList.remove("active");
+    if (li.classList.contains(current)) {
+      li.classList.add("active");
+    }
+  });
+};
+
